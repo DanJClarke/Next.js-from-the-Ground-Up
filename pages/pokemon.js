ec126fb5d717c04a0f6551aa8bc86b70fa87e2b6
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2021-03-27 23:48:16 +0000

    use the pokemon api

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,5 +1,22 @@
-function Pokemon () {
-    return <div>Welcome, Pokemon Fan!</div>
+function Pokemon ({ pokemon }) {
+    return (
+        <div>
+            Welcome { pokemon?.name }!
+            <img src={ pokemon?.sprites.front_default } />
+        </div>
+    )
 }
+
+export async function getStaticProps() {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon/charmander');
+    const pokemon = await res.json();
+
+    return {
+        props: {
+            pokemon
+        }
+    }
+}
+
 
 export default Pokemon
